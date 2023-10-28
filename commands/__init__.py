a37102bd4c55c58d20ca41f1359cb9a4ddac3b6a
Sebastian <sebastianbengtsson@me.com>
2023-10-29 00:56:43 +0200

    Refactoring of 2023: All commands are now in separate files

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,8 +1,21 @@
-from ziggotalk import ziggotalk
+from commands.ziggotalk import ziggotalk
+from commands.bomba import bomba
+from commands.pingziggo import pingziggo
+from commands.translate import translate
+from commands.help_command import help_command
 
-commands = {}
+all_commands = {}
+
 
 def add_command(command) -> None:
-    commands[command.get_name()] = command
+    all_commands["/" + command.get_name()] = command
+
 
 add_command(ziggotalk)
+add_command(bomba)
+add_command(pingziggo)
+add_command(translate)
+add_command(help_command)
+
+if __name__ == '__main__':
+    print(all_commands.keys())
