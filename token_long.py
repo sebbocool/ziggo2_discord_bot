c31f6a0e68b82d5f6a904baf9141ebc1f271b535
Sebastian <sebastianbengtsson@me.com>
2023-10-29 13:36:43 +0100

    fixed security issues

--- token_long.py
+++ token_long.py
@@ -1,13 +1,7 @@
 from os import environ
 
-testing = True
-
-
 # Discord
 TOKEN = environ.get("DISCORD_TOKEN_ZIGGO2")
-
-if testing:
-    TOKEN = "MTE2Njg0MTY4MTg0NTQ0MDU4Mw.GBGHIJ.91B7yz7gbthR4vH9Zetpt2OUp3dD8oRrM0iF0I"
 
 # DeepL
 DEEPLTOKEN = environ.get("DEEPL_TOKEN")
