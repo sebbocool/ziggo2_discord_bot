bb0f8841e8582951070063df826bc48f6a3a4ba2
Thorsten <thorstenrausch1@gmail.com>
2023-10-29 02:05:04 +0200

    Extract functions for on_message

--- bot.py
+++ bot.py
@@ -2,7 +2,16 @@ import discord
 import random
 
 from ziggo2_discord_bot.token_long import TOKEN
-from ziggo2_discord_bot.commands import all_commands as cmds
+from ziggo2_discord_bot.commands import run_command, COMMAND_PREFIX
+
+
+async def add_random_reaction(message):
+    beast_emoji = discord.utils.get(message.guild.emojis, name="beast")
+    cringe_emoji = discord.utils.get(message.guild.emojis, name="cringe")
+    ziggo_emoji = discord.utils.get(message.guild.emojis, name="ziggo")
+    emoji_to_use = random.choice([beast_emoji, cringe_emoji, ziggo_emoji])
+    if emoji_to_use:
+        await message.add_reaction(emoji_to_use)
 
 
 class ziggo2(discord.Client):
@@ -10,7 +19,6 @@ class ziggo2(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
-    ### READ MESSAGES ###
     async def on_message(self, message):
         if message.author == self.user:
             return
@@ -20,25 +28,11 @@ class ziggo2(discord.Client):
             await message.channel.send("https://media.tenor.com/uFeEjGFzmc0AAAAC/stop.gif", reference=message)
             return
 
-        ### sometimes react with a random emoji
         if random.randint(1, 12) == 6:
-            beast_emoji = discord.utils.get(message.guild.emojis, name="beast")
-            cringe_emoji = discord.utils.get(message.guild.emojis, name="cringe")
-            ziggo_emoji = discord.utils.get(message.guild.emojis, name="ziggo")
+            await add_random_reaction(message)
 
-            emoji_to_use = random.choice([beast_emoji, cringe_emoji, ziggo_emoji])
-            if emoji_to_use:
-                await message.add_reaction(emoji_to_use)
-
-        ### check if message is a command
-        if message.content.startswith("/"):
-            name, *arg = message.content.strip().split(" ", 1)
-            if name in cmds:
-                arg = arg[0] if arg else None
-                if arg:
-                    await cmds[name].run(msg=message, arg=arg)
-                else:
-                    await cmds[name].run(msg=message)
+        if message.content.startswith(COMMAND_PREFIX):
+            await run_command(message)
 
 
 intents = discord.Intents.default()
--- commands/__init__.py
+++ commands/__init__.py
@@ -1,14 +1,18 @@
+import discord
+
 from ziggo2_discord_bot.commands.ziggotalk import ziggotalk
 from ziggo2_discord_bot.commands.bomba import bomba
 from ziggo2_discord_bot.commands.pingziggo import pingziggo
 from ziggo2_discord_bot.commands.translate import translate
 from ziggo2_discord_bot.commands.help_command import help_command
 
+COMMAND_PREFIX = "/"
+
 all_commands = {}
 
 
 def add_command(command) -> None:
-    all_commands["/" + command.get_name()] = command
+    all_commands[COMMAND_PREFIX + command.get_name()] = command
 
 
 add_command(ziggotalk)
@@ -16,3 +20,13 @@ add_command(bomba)
 add_command(pingziggo)
 add_command(translate)
 add_command(help_command)
+
+
+async def run_command(message: discord.Message):
+    name, *arg = message.content.strip().split(" ", len(COMMAND_PREFIX))
+    if name in all_commands:
+        arg = arg[0] if arg else None
+        if arg:
+            await all_commands[name].run(msg=message, arg=arg)
+        else:
+            await all_commands[name].run(msg=message)
