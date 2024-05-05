a6a4a724ea96ce85417f2a262555a456cc2b88c3
Thorsten <thorstenrausch1@gmail.com>
2024-05-06 00:26:08 +0200

    Make friend command chattier

--- commands/friend.py
+++ commands/friend.py
@@ -1,23 +1,75 @@
+import random
+
 import discord
 
 from commands.command import Command
 
 
-async def friend(msg: discord.Message, _arg: str | None):
-    channel = msg.author.voice.channel
+def get_random_cannot_find_message() -> str:
+    return random.choice([
+        "I can't find you...",
+        "I cannot find you...",
+        "I can't find you :(",
+        "I cannot find you :(",
+        "Where are you?",
+    ])
 
-    if channel is None:
-        await msg.channel.send("I'm sorry, but I cannot find that voice channel.")
+
+def get_random_join_message() -> str:
+    return random.choice([
+        "I'll be there",
+        "I can talk for a bit",
+        "Coming",
+        "On my way",
+        "Sure",
+        "I'll be right there",
+        "I'm here'",
+        "Hello",
+        "Helloo",
+        "Hi",
+        "Hii",
+    ]) + random.choice([
+        "",
+        "!",
+        "!!",
+        "! :)",
+        " :)",
+        "! :3",
+        " :3",
+        " ᗜˬᗜ",
+    ])
+
+
+def get_random_refuse_message() -> str:
+    return random.choice([
+        "No, sorry.",
+        "I'm busy.",
+        "I'm busy, sorry.",
+        "No thanks.",
+        "Ew?",
+    ])
+
+
+async def friend(msg: discord.Message, _arg: str | None):
+    voice = msg.author.voice
+
+    if voice is None or voice.channel is None:
+        await msg.channel.send(get_random_cannot_find_message())
+        return
+
+    if random.randint(1, 18) == 1:
+        await msg.channel.send(get_random_refuse_message())
         return
 
     if msg.channel.guild.voice_client is not None:
         await msg.channel.guild.voice_client.disconnect(force=True)
 
-    await channel.connect()
+    await msg.channel.send(get_random_join_message())
+    await voice.channel.connect()
 
 
 friend = Command(
     name="friend",
-    description="Ask ziggo2 to hangout with you.",
+    description="Ask ziggo2 to hang out with you.",
     func=friend
 )
